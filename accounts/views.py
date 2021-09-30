df6a06761bd0aa404db486190e20b85bb72e535f
xer <xer@xer-labtop>
2021-10-01 01:37:00 +0300

    accounts/:able to change page title

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,10 @@ class SignupView(CreateView):
     form_class = UserSignupForm
     template_name = 'signup.html'
     success_url = reverse_lazy('login')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'signup'
+        return context
 
 
 def profile_view(request):
