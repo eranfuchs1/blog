3b254bf274602f2533e92686438a971d4a9c15f0
xer <xer@xer-labtop>
2021-10-03 20:05:22 +0300

    setting placeholder widgets for signup instead of using labels

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserSignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserSignupForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'username'
+        self.fields['password1'].widget.attrs['placeholder'] = 'password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'password confirm'
     class Meta:
         model = User
-        fields = ['username', ]
+        fields = ['username', 'password1', 'password2']
